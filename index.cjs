38f566cc3a6fc4e020b1d4f4a3e947cb73814b8a
alelikak <141032811+alelikak@users.noreply.github.com>
2023-10-10 04:51:59 -0400

    Add files via upload

--- index.cjs
+++ index.cjs
@@ -174,7 +174,7 @@ app.get('/api/persons/:id', (request, response) => {
 
 
 //const PORT = process.env.PORT || 3001
-const PORT = 10000
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
