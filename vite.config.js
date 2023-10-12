c552bf4c5bc45e9c8d5f7f2122016abf05b0a2ce
alelikak <141032811+alelikak@users.noreply.github.com>
2023-10-12 02:59:38 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {    
     proxy: {      
       '/api': {       
-         target: 'http://localhost:3001',        
+         target: 'render-test-y9v7.onrender.com/',        
          changeOrigin: true,      
         },    
       }  }
